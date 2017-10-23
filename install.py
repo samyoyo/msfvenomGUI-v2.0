7543af429a83e9e3eec0dc55bc01b9baa365f2e8
Black-rOOt <mmkkjjmm55555@gmail.com>
2017-10-23 19:43:20 +0300

    Update install.py

--- install.py
+++ install.py
@@ -4,15 +4,15 @@ try:
     {1} > Linux (Ubuntu and Debian-based)
     {2} > Linux (CentOS and RPM-based)
     """
-    numper = input("Enter the numper >: ")
-    if numper == 1:
+    number = input("Enter the number >: ")
+    if number == 1:
         os.system("sudo apt-get install python-pyside")
         os.system("sudo apt-get install python-qt4")
         os.system("mkdir /opt/msfvenomGUI")
         os.system("cp msfvenomGUIv2.0.py /opt/msfvenomGUI/")
         os.system("chmod +x msfvenomGUI")
         os.system("cp msfvenomGUI /usr/bin/")
-    elif numper == 2:
+    elif number == 2:
         os.system("yum install python-pyside pyside-tools")
         os.system("yum install PyQt4")
         os.system("mkdir /opt/msfvenomGUI")
